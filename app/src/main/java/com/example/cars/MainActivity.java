229c7ef4c9fd958873ddc10d1419379c9c08fcad
Roy C Wright <roy.c.wright05@gmail.com>
2019-10-18 10:47:26 -0700

    Home view goes to recycler view with no adapter implemented.

--- app/src/main/java/com/example/cars/MainActivity.java
+++ app/src/main/java/com/example/cars/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.cars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        mButton = view.findViewById(R.id.btnEnter);
+        Intent intent = new Intent(this, Main2Activity.class);
+
+        startActivity(intent);
+
     }
 }

d91d04a5e161e3388cc5a6f797b09b23357281a0
Roy C Wright <roy.c.wright05@gmail.com>
2019-10-20 18:44:55 -0700

    Commit changes, merge to remote master.  Make changes in cloned project.

---
+++ app/src/main/java/com/example/cars/Model/CarBodyTypes.java
@@ -0,0 +1,42 @@
+package com.example.cars.Model;
+
+public class CarBodyTypes {
+
+    private int smallImage;
+    private String carBodyType;
+    private String carCategory;
+
+    public CarBodyTypes() {
+
+    }
+
+    public CarBodyTypes(int smallImage, String carBodyType, String carCategory) {
+        this.smallImage = smallImage;
+        this.carBodyType = carBodyType;
+        this.carCategory = carCategory;
+    }
+
+    public int getSmallImage() {
+        return smallImage;
+    }
+
+    public void setSmallImage(int smallImage) {
+        this.smallImage = smallImage;
+    }
+
+    public String getCarBodyType() {
+        return carBodyType;
+    }
+
+    public void setCarBodyType(String carBodyType) {
+        this.carBodyType = carBodyType;
+    }
+
+    public String getCarCategory() {
+        return carCategory;
+    }
+
+    public void setCarCategory(String carCategory) {
+        this.carCategory = carCategory;
+    }
+}
--- app/src/main/java/com/example/cars/View/MainActivity.java
+++ app/src/main/java/com/example/cars/View/MainActivity.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.cars.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mButton;
+    private ImageButton mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
